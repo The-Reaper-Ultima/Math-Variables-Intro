a9682f4588dbd27c4899dcff29022af046c5b3ff
The-Reaper-Ultima <ru1496@pleasantonusd.net>
2025-08-18 10:28:56 -0700

    1

---
+++ Math-Variables-Intro-main (1)/Math-Variables-Intro-main/Main.java
@@ -0,0 +1,61 @@
+class Main {
+  public static void main(String[] args) {
+
+    //Use the // to create single line comments to either add info or to take out code for debugging
+
+    System.out.println("Hello world!");
+    
+    //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
+
+
+
+
+
+
+    //ANSWER: println prints the text and makes subsequent text appear on the next line, print doesn't do this
+
+
+    //Throughout the year this year we will need to store information. For that we will be using VARIABLES!
+
+    //Java is an Object-Oriented programming language. All variables we use this year will either be OBJECTS or PRIMITIVES
+
+    //There are 8 primitives in Java: int, byte, short, long, float, double, boolean, char
+
+    //For AP we need to know: int, double, boolean
+
+    //List examples of the types below and give definition
+    //int (integer): a non decimal number
+    //double: a floating point number
+    //boolean: true or false
+
+    //For now we are just going to work with primitive
+
+    //Create 3 variables of each of the above types (USE GOOD CODING PRACTICE WHEN CREATING THE VARIABLES
+int grade,age, cars = 67;
+grade = age = cars;
+double gpa, weight, double2 = 5.00;
+boolean bool1, bool2 = true;
+
+System.out.println(grade);
+
+    //MATH TIME!
+
+    //What are the math operators that we can use?
+    //+ - * / %
+
+    //Try doing some math operations with numbers. How can we check to see if the math worked?
+
+    //Create codes that will print the following:
+
+    //Odd integers from 1 to 100, inclusive of both
+
+
+
+    //All multiples of 3 from 1 to 100
+
+
+
+
+    //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
+  }
+}
